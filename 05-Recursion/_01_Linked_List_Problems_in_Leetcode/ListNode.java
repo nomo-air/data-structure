a79a0af4f1d349322582bd3c11b619dd513a2e99
zhengwanlei <zhengwanlei@nibirutech.com>
2018-10-17 18:04:23 +0800

    feat.Leetcode中和链表相关的问题

---
+++ 05-Recursion/_01_Linked_List_Problems_in_Leetcode/ListNode.java
@@ -0,0 +1,12 @@
+package _01_Linked_List_Problems_in_Leetcode;
+
+//Definition for singly-linked list.
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
