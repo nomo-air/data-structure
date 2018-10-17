58d1f95a50a8e613200de538bc12e3244541fd79
zhengwanlei <zhengwanlei@nibirutech.com>
2018-10-17 11:06:00 +0800

    feat.使用链表的虚拟头节点

--- 04-Linked-List/_01_Linked_List_Basics/Main.java
+++
@@ -1,8 +0,0 @@
-package _01_Linked_List_Basics;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- 04-Linked-List/_02_Add_Elements_in_LinkedList/Main.java
+++
@@ -1,8 +0,0 @@
-package _02_Add_Elements_in_LinkedList;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
