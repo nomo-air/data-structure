9b84a5c6b6b6fb4de01968164c6ee04361239d19
郑万雷 <zhengwl@codoon.com>
2019-03-12 17:09:41 +0800

    feat.不浪费那1个空间

--- src/queues/LoopQueue1.java
+++ src/queues/LoopQueue1.java
@@ -1,20 +1,19 @@
 package queues;
-
-// 不浪费那1个空间
-public class LoopQueue<E> implements Queue<E> {
-
+// 在这一版LoopQueue的实现中，我们将不浪费那1个空间：）
+public class LoopQueue1<E> implements Queue<E> {
     private E[] data;
     private int front, tail;
     private int size;
 
-    public LoopQueue(int capacity) {
-        data = (E[]) new Object[capacity]; // 由于不浪费空间，所以data静态数组的大小是capacity而不是capacity + 1
+    public LoopQueue1(int capacity) {
+        data = (E[]) new Object[capacity]; // 由于不浪费空间，所以data静态数组的大小是capacity
+        // 而不是capacity + 1
         front = 0;
         tail = 0;
         size = 0;
     }
 
-    public LoopQueue() {
+    public LoopQueue1() {
         this(10);
     }
 
@@ -35,10 +34,11 @@ public class LoopQueue<E> implements Queue<E> {
 
     @Override
     public void enqueue(E e) {
+
         // 注意，我们不再使用front和tail之间的关系来判断队列是否为满，而直接使用size
-        if (size == getCapacity()) {
+        if (size == getCapacity())
             resize(getCapacity() * 2);
-        }
+
         data[tail] = e;
         tail = (tail + 1) % data.length;
         size++;
@@ -46,32 +46,32 @@ public class LoopQueue<E> implements Queue<E> {
 
     @Override
     public E dequeue() {
-        if (isEmpty()) {
+
+        if (isEmpty())
             throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
-        }
+
         E ret = data[front];
         data[front] = null;
         front = (front + 1) % data.length;
         size--;
-        if (size == getCapacity() / 4 && getCapacity() / 2 != 0) {
+        if (size == getCapacity() / 4 && getCapacity() / 2 != 0)
             resize(getCapacity() / 2);
-        }
         return ret;
     }
 
     @Override
     public E getFront() {
-        if (isEmpty()) {
+        if (isEmpty())
             throw new IllegalArgumentException("Queue is empty.");
-        }
         return data[front];
     }
 
     private void resize(int newCapacity) {
+
         E[] newData = (E[]) new Object[newCapacity];
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++)
             newData[i] = data[(i + front) % data.length];
-        }
+
         data = newData;
         front = 0;
         tail = size;
@@ -85,11 +85,11 @@ public class LoopQueue<E> implements Queue<E> {
         // 注意，我们的循环遍历打印队列的逻辑也有相应的更改 :-)
         for (int i = 0; i < size; i++) {
             res.append(data[(front + i) % data.length]);
-            if ((i + front + 1) % data.length != tail) {
+            if ((i + front + 1) % data.length != tail)
                 res.append(", ");
-            }
         }
         res.append("] tail");
         return res.toString();
     }
+
 }
