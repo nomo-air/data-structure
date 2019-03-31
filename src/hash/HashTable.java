2bf2d0c39b7da2470bea0991320956a50caf47dc
郑万雷 <zhengwl@codoon.com>
2019-03-31 21:59:20 +0800

    feat.Trie字典树

--- src/hash/HashTable.java
+++ src/hash/HashTable.java
@@ -4,10 +4,11 @@ import java.util.TreeMap;
 
 public class HashTable<K extends Comparable<K>, V> {
 
-    private final int[] capacity
-            = {53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593,
+    private final int[] capacity = {
+            53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593,
             49157, 98317, 196613, 393241, 786433, 1572869, 3145739, 6291469,
-            12582917, 25165843, 50331653, 100663319, 201326611, 402653189, 805306457, 1610612741};
+            12582917, 25165843, 50331653, 100663319, 201326611, 402653189, 805306457, 1610612741
+    };
 
     private static final int upperTol = 10;  // 每一个哈希地址冲突的上界
     private static final int lowerTol = 2;
